4125a37d954536f86e0d9fdfee1cc5865b02eb67
jaygroover1 <jaygroover@gmail.com>
2019-03-09 01:32:14 -0500

    new update

---
+++ assets/javascript/game.js
@@ -0,0 +1,149 @@
+var wordList = [
+"hulk",
+"thor",
+"captainamerica",
+"marvel",
+"antman"
+]
+
+var chosenWord = "";
+var letterInChosenWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongGuesses = [];
+
+var winCounter = 0;
+var lossCounter = 1;
+var numGuesses = 15;
+
+function startGame(){
+    
+    
+    
+    
+    
+    
+    
+    
+    wrongGuessess =[];
+    console.log("this is wrong guesses in startGame", wrongGuesses);
+    numGuesses = 15;
+    blanksAndSuccesses = [];
+
+    
+    chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+    lettersInChosenWord = chosenWord.split("");
+    numBlanks = lettersInChosenWord.length;
+    console.log(chosenWord);
+    console.log(numBlanks);
+
+    for(var i = 0; i < numBlanks; i++){
+        blanksAndSuccesses.push("_");
+    }
+    console.log(blanksAndSuccesses);
+    document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
+document.getElementById('guesses-left').innerHTML = numGuesses;
+
+
+
+}
+
+
+function checkLetters(letter){
+   
+    
+    
+    
+    
+    
+    
+    var letterInWord = false;
+
+    for(var i = 0; i < numBlanks; i++){
+        if(chosenWord[i] === letter){
+            letterInWord = true;
+        
+        }
+    }
+
+    if(letterInWord){
+        for(i = 0; i < numBlanks; i++){
+            if(chosenWord[i] === letter){
+                blanksAndSuccesses[i] = letter;
+            
+            }
+        
+        }
+    }else{
+        numGuesses --;
+        wrongGuesses.push(letter)
+    }
+
+
+
+
+
+
+
+
+}
+   
+   
+   function roundComplete(){
+      
+   
+       
+    
+    
+    
+    
+    document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
+       document.getElementById('guesses-left').innerHTML = numGuesses;
+       document.getElementById('wrong-guesses').innerHTML = wrongGuesses.join(" ");
+   
+   
+       // if(blanksAndSuccesses.indexOf(letter >= 1)){
+       //     console.log(letter)
+       // }
+       console.log(lettersInChosenWord);
+       console.log(blanksAndSuccesses);
+       if(lettersInChosenWord.join(" ") === blanksAndSuccesses.join(" ")){
+           winCounter++;
+           alert("You win!!");
+           document.getElementById('win-counter').innerHTML = winCounter;
+           startGame();
+       }else if(numGuesses === 0){
+           document.getElementById('loss-counter').innerHTML  = lossCounter ++;
+           document.getElementById('wrong-guesses').innerHTML = "";
+           alert("you don't have any more guesses");        
+           startGame();
+       }
+   
+   
+   
+   
+   }
+   startGame();
+   document.onkeyup = function(event){
+       /*
+       1. its going to take in the letter that we type in
+       2. its going to pass it through the CheckLetter function 
+       */
+       var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+       console.log("this is the letter we typed", letterGuessed)
+       checkLetters(letterGuessed)
+       roundComplete();
+   
+   
+   }
+
+
+
+
+
+
+
+
+
+
+
